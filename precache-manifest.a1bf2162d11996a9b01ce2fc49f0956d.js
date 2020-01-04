12ec907547ab0c56f9219ec5db0a52dda7715b9d
Gabriela Olivares <gbs@MacBook-Pro-de-Gabriela.local>
2020-01-03 21:55:05 -0600

    Updates

---
+++ precache-manifest.a1bf2162d11996a9b01ce2fc49f0956d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d767d67b9a71fc4d5f3c0c9420df571d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "13272334192dd2d0b955",
+    "url": "/monsters-rolodex/static/css/main.71e96536.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13272334192dd2d0b955",
+    "url": "/monsters-rolodex/static/js/main.df07e97f.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
